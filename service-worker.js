d59e271afdf6e1b1d7b5fb7f449cbb39ce76f605
G44T <102338873+g44tsec@users.noreply.github.com>
2024-10-21 13:19:30 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,22 @@
-// Service Worker
-
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('game-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/style.css',
-                '/script.js',
-                '/manifest.json',
-                'Neurive.png'
-            ]);
-        })
+      caches.open('game-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/style.css',
+          '/script.js',
+          '/manifest.json',
+          'Neurive.png'
+        ]);
+      })
     );
-});
-
-self.addEventListener('fetch', function(event) {
+  });
+  
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
     );
-});
-
-// Add display rules for hub-container and intro-hub in cache
-self.addEventListener('message', function(event) {
-    if (event.data === 'applyCSS') {
-        applyInitialCSS();
-    }
-});
-
-function applyInitialCSS() {
-    const styleSheet = new CSSStyleSheet();
-    styleSheet.replaceSync(`
-        .hub-container {
-            display: none; /* Hide the game hub initially */
-        }
-
-        #intro-hub {
-            display: block; /* Show the intro hub initially */
-        }
-    `);
-
-    document.adoptedStyleSheets = [styleSheet];
-}
+  });
+  
